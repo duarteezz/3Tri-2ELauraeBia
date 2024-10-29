5f15bdddba264eb7d142a8a49788963fe4003ba3
duarteezz <160139359+duarteezz@users.noreply.github.com>
2024-10-29 08:51:44 -0300

    29/10
    29/10

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-function criaCartao(categoria, pergunta, resposta){
-   let container = document.getElementById ('container')
+function criaCartao(categoria, pergunta, resposta) {
+   let container = document.getElementById('container')
    let article = document.createElement('article')
    article.className = 'cartao'
-   article.innerHTML = 'Isso é um teste'
+   article.innerHTML = ` 
+      <div class= "cartao-conteudo">
+         <h3> ${categoria} </h3>
+         <div class="cartao-conteudo-pergunta">
+            <p> ${pergunta} </p>
+         </div>
+         <div class="cartao-conteudo-resposta">
+            <p> ${resposta} </p>
+         </div>
+      </div>
+   `
    container.appendChild(article)
 }
 
-criaCartao("Biologia", "Qual a função da mitocondria", "Produção de energia (ATP)")
-
+criaCartao("Biologia", "Qual a função da mitocôndria?", "Produção de energia (ATP)")
+criaCartao("História", "O que foi a idade média?","Ficou marcada pelo feudalismo, pela influencia da igreja católica e pelas cruzadas e inquisições ")
